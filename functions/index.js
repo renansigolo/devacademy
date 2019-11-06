3c0ab30c81ac9611ff0bdae9085c030e808976ca
Renan Sigolo <renan.sigolo@gmail.com>
2019-11-06 18:43:13 +1100

    add email template

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions'),
   nodemailer = require('nodemailer'),
   admin = require('firebase-admin'),
+  fs = require('fs'),
   cors = require('cors')({ origin: true })
 
 admin.initializeApp()
@@ -26,38 +27,50 @@ exports.sendEmail = functions.https.onRequest((req, res) => {
       email: req.body.email
     }
 
-    const mailOptions = {
-      subject: 'DevAcademy - New Subscriber (Test)',
-      from: '"DevAcademy Website" <no-reply@devacademy.com.au>',
-      to: 'renan@devacademy.com.au'
-    }
+    fs.readFile('templates/welcome.html', { encoding: 'utf-8' }, function (err, data) {
+      // data = data.toString()
 
-    // Build Email message.
-    const htmlMessage = `
-    <p>New DevAcademy Subscriber</p>
-    <p><strong>Name:</strong> ${formPayload.firstName} ${formPayload.lastName}</p>
-    <p><strong>First Name:</strong> ${formPayload.firstName}</p>
-    <p><strong>Last Name:</strong> ${formPayload.lastName}</p>
-    <p><strong>Email:</strong> ${formPayload.email}</p>
-    `
+      const textMessage = `
+      New DevAcademy Subscriber
+      Name: ${formPayload.firstName} ${formPayload.lastName}
+      First Name: ${formPayload.firstName}
+      Last Name: ${formPayload.lastName}
+      Email: ${formPayload.email}
+      `
 
-    // const textMessage = `
-    // New DevAcademy Subscriber
-    // Name: ${formPayload.firstName} ${formPayload.lastName}
-    // First Name: ${formPayload.firstName}
-    // Last Name: ${formPayload.lastName}
-    // Email: ${formPayload.email}
-    // `
+      // Build Email message.
+      const htmlMessage = `
+      <p>New DevAcademy Subscriber</p>
+      <p><strong>Name:</strong> ${formPayload.firstName} ${formPayload.lastName}</p>
+      <p><strong>First Name:</strong> ${formPayload.firstName}</p>
+      <p><strong>Last Name:</strong> ${formPayload.lastName}</p>
+      <p><strong>Email:</strong> ${formPayload.email}</p>
+      `
 
-    mailOptions.html = htmlMessage
-    mailOptions.text = textMessage
+      const mailOptions = {
+        subject: 'DevAcademy - New Subscriber',
+        from: '"DevAcademy Website" <no-reply@devacademy.com.au>',
+        to: 'renan@devacademy.com.au',
+        html: htmlMessage,
+        text: textMessage
+      }
 
-    return mailTransport
-      .sendMail(mailOptions)
-      .then(() => res.status(200).end())
-      .catch(error => {
+      try {
+        return mailTransport.sendMail(mailOptions)
+          .then(() => res.status(200).end())
+      } catch (error) {
         console.error('There was an error while sending the email:', error)
-        res.status(500).send(error)
-      })
+        return res.status(500).send(error)
+      }
+    })
+
+    // return mailTransport
+    //   .sendMail(mailOptions)
+    //   .then(() => res.status(200).end())
+    //   .catch(error => {
+    //     console.error('There was an error while sending the email:', error)
+    //     res.status(500).send(error)
+    //   })
+
   })
 })
