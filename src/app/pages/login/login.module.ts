7dc6ebc712d26c5cc14c6b2df408a130dae21837
Renan Sigolo <renan.sigolo@gmail.com>
2019-11-07 13:31:00 +1100

    Add elements to the dashboard

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common'
 import { LoginComponent } from './login.component'
 import { Routes, RouterModule } from '@angular/router'
 import { SharedModule } from 'app/shared/shared.module'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 const routes: Routes = [{ path: '', component: LoginComponent }]
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), SharedModule]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule,
+    MatTabsModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule
+  ]
 })
 export class LoginModule {}
