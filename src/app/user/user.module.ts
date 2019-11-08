1f6292411c40594dd448c37afda2b09ad8cd9b40
Renan Sigolo <renan.sigolo@gmail.com>
2019-11-08 14:06:26 +1100

    Adjust Login Page

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { UserRoutingModule } from './user-routing.module'
-import { LoginPageComponent } from './login-page/login-page.component'
-import { GoogleSigninDirective } from './google-signin.directive'
-import { SharedModule } from 'app/shared/shared.module'
-import { MatTabsModule } from '@angular/material/tabs'
+import { NgModule } from '@angular/core'
 import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatTabsModule } from '@angular/material/tabs'
+import { SharedModule } from 'app/shared/shared.module'
+import { EmailLoginComponent } from './email-login/email-login.component'
+import { GoogleSigninDirective } from './google-signin.directive'
+import { LoginPageComponent } from './login-page/login-page.component'
+import { UserRoutingModule } from './user-routing.module'
 
 @NgModule({
-  declarations: [LoginPageComponent, GoogleSigninDirective],
+  declarations: [
+    LoginPageComponent,
+    EmailLoginComponent,
+    GoogleSigninDirective
+  ],
   imports: [
     CommonModule,
     UserRoutingModule,
